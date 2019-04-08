0b1c16299909240ea8b130926e7d9123f946c3fe
DV <DVislearning@gmail.com>
2019-04-08 01:25:58 -0700

    Updates

---
+++ precache-manifest.5b61977379ea3bfe51c0443caadde298.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cb8bc2021af5db4ec66",
+    "url": "/simpsons-quote-machine/static/css/main.6176458e.chunk.css"
+  },
+  {
+    "revision": "2cb8bc2021af5db4ec66",
+    "url": "/simpsons-quote-machine/static/js/main.b40051ef.chunk.js"
+  },
+  {
+    "revision": "a2fe9bd888c5d059790c",
+    "url": "/simpsons-quote-machine/static/js/runtime~main.88d57eb2.js"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/simpsons-quote-machine/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "c55c0c3666be321dbaad84efee2054fe",
+    "url": "/simpsons-quote-machine/index.html"
+  }
+];
